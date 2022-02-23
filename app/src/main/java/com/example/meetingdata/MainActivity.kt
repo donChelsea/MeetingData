d4890b0f8c230586f20833dc58473edcc9d09858
Chelsea Katsidzira <chelsea.katsidzira@bounteous.com>
2022-02-23 17:55:40 -0500

    cleaned up code

--- app/src/main/java/com/example/meetingdata/MainActivity.kt
+++ app/src/main/java/com/example/meetingdata/MainActivity.kt
@@ -1,27 +1,49 @@
 package com.example.meetingdata
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import com.example.meetingdata.models.Meeting
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.example.meetingdata.databinding.ActivityMainBinding
 import com.example.meetingdata.models.User
+import java.time.Instant
+import java.time.ZoneId
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val meetingMap = mutableMapOf<String, MutableList<User>>()
+    private lateinit var binding: ActivityMainBinding
 
+    var max = 0
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         readFromAsset()
+
+        showData()
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun showData() {
+        binding.apply {
+            maxUsersValue.text = findMax()
+            minUsersValue.text = findMin()
+            uniqueUsersValue.text = "Unique Users: ${getUniqueNumberOfUsers()}"
+        }
     }
 
     private fun readFromAsset() {
         val fileName = "meetingdata.txt"
         val bufferReader = application.assets.open(fileName).bufferedReader()
         bufferReader.useLines { str ->
+            var meetingId = ""
             str.forEach {
-                var meetingId = ""
                 val split = it.split(" ")
                 if (split.size == 1) {
                     meetingId = split[0]
@@ -34,8 +56,47 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        Log.d("readFromAsset", "values" + meetingMap.values.toString())
-        Log.d("readFromAsset", "keys" + meetingMap.keys.toString())
+    }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun findMax(): String {
+        var time = ""
+        meetingMap.forEach { v, u ->
+            if (u.size > max) {
+                max = u.size
+                time = v
+            }
+        }
+        return "Time: ${getDateTime(time)} - $max"
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun findMin(): String {
+        var min = max
+        var time = ""
+        meetingMap.forEach { v, u ->
+            if (u.size < min && u.size > 0) {
+                min = u.size
+                time = v
+            }
+        }
+        return "Time: ${getDateTime(time)} - $min"
+    }
+
+    fun getUniqueNumberOfUsers(): Int {
+        val userSet = mutableSetOf<User>()
+        meetingMap.forEach { v, u ->
+            userSet.addAll(u)
+        }
+        return userSet.size
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getDateTime(time: String): String {
+        val dt = Instant.ofEpochSecond(time.toLong())
+            .atZone(ZoneId.systemDefault())
+            .toLocalDateTime()
+        val time = dt.toString().split("T")
+        return time.last()
     }
 }
